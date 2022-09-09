0305e68315133f6a8588d899c1cb9a737c534a4f
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-09-09 23:21:10 +0700

    create database and root user

--- src/main/java/OrderManagementApplication.java
+++ src/main/java/OrderManagementApplication.java
@@ -1,5 +1,34 @@
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class OrderManagementApplication {
+
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:src/main/resources/" + fileName;
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("The db path is " + conn.getMetaData().getURL());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Create failed");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        createNewDatabase("OrderManagementSystem.db");
     }
 }
--- src/main/java/helper/DbConnection.java
+++ src/main/java/helper/DbConnection.java
@@ -1,4 +1,15 @@
 package helper;
 
 public class DbConnection {
+
+    private static String url;
+    private static String username;
+    private static String password;
+
+    private static DbConnection instance = null;
+
+    private DbConnection() {
+
+    }
+
 }
